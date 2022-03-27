4512bd05c44d4e2b59c4a69009d3cef5ea126c0a
Алексей Садовой <sapoko.stv@yandex.ru>
2022-03-27 10:23:23 +0300

    Initial commit

---
+++ id_company.py
@@ -0,0 +1,47 @@
+import requests
+from config import access_token
+from pathlib import Path
+
+url_company = 'https://sadovoya.amocrm.ru/api/v4/companies'
+headers = {'Authorization': f"Bearer {access_token}"}
+
+
+class IdCompany:
+    def __init__(self, url=url_company, header=headers):
+        self.url = url
+        self.headers = header
+        self.response = requests.get(self.url, headers=self.headers)
+
+    def access_data(self):
+        self.result = self.response.json()
+        return self.result
+
+    def show_status_cod(self):
+        print(self.response.status_code)
+
+
+class Dir:
+    def __init__(self, idcompany, col=0):
+        self.idcompany = idcompany
+        self.col = col
+
+    def show_id(self):
+        data_dict = self.idcompany['_embedded']['companies']
+        col = 0
+        for i in data_dict:
+            try:
+                Path(f"/home/hortus/Documents/bases/firms/{i['id']}").mkdir(parents=True, exist_ok=False)
+            except OSError:
+                print(f"Каталог с номером клиента - {i['id']} существует")
+            else:
+                col += 1
+        print(f'Создание каталогов завершено, Всего создано {col}')
+
+        
+a = IdCompany()
+a.show_status_cod()
+b = Dir(a.access_data())
+b.show_id()
+
+
+
