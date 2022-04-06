eb799e90260b6ac4dba427873b2e0537aac7211b
Алексей Садовой <sapoko.stv@yandex.ru>
2022-04-06 17:18:16 +0300

    Прверка картиной по 1 строке

--- SpecialFormat.py
+++ SpecialFormat.py
@@ -1,9 +1,9 @@
 import json
-import pprint
 import re
 import sqlite3
-
+from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
+import requests
 
 path_to_base = '/home/hortus/PycharmProjects/amocrm/app.db'
 
@@ -16,6 +16,7 @@ class SearchArticles:
         self.list_body = []
         self.count = 0
         self.result = None
+        self.protocol=[]
 
     def execute_one(self, article_id):
         self.article_id = article_id
@@ -23,85 +24,100 @@ class SearchArticles:
         self.result = self.cursor.fetchone()
         return self.result
 
-    def get_article_content_all(self,):
-        self.cursor.execute("""SELECT a.date, ac.content FROM cms_article_content ac
-                            LEFT JOIN cms_articles a on a.article_id = ac.article_id
-                            WHERE a.article_id""")
-        return self.cursor.fetchall()
+    def execute_all(self,):
+        self.cursor.execute("SELECT content FROM cms_article_content")
+        self.result = self.cursor.fetchall()
+        return self.result
 
     def get_article_content(self):
         self.cursor.execute("""SELECT a.date, ac.content FROM cms_article_content ac
-                            LEFT JOIN cms_articles a on a.article_id = ac.article_id
-                            WHERE a.article_id = 7""")
+                                LEFT JOIN cms_articles a on a.article_id = ac.article_id
+                                WHERE a.article_id = 7""")
         return self.cursor.fetchone()
 
-    def parse_content(self, content):
-        for i in self.get_article_content_all():
+    def parse_content(self, date, content):
+        clr = re.sub(r"[\\\r\\\n]", "", content)
+        data_string = BeautifulSoup(clr, 'lxml')
 
-            clr = re.sub(r"[\\\r\\\n]", "", content[1])
-            data_string = BeautifulSoup(clr, 'lxml')
-
-            for i in data_string.body:
-                if i.name == 'p':
-                    self.list_body.append({
-                            "type": "paragraph",
-                            "data": {
-                                "text": str(i.text),
-                                }
-                    })
-                elif i.name == 'figure':
-                    try:
-                        self.list_body.append(
-                            {
-                                "type": "image",
-                                "data": {
-                                    "file": {
-                                        "url": str(i.contents[0].attrs['src'])
-                                    },
-                                    "caption": "need to be correct parse",
-                                    "withBorder": "false",
-                                    "withBackground": "false",
-                                    "stretched": "true"
-                                }
+        for i in data_string.body:
+            if i.name == 'p':
+                self.list_body.append({
+                        "type": "paragraph",
+                        "data": {
+                            "text": str(i.text),
                             }
-                                              )
-                    except IndexError:
-                        print(f'отсутсвует значение{i}')
-                elif i.name == 'h2':
+                })
+            elif i.name == 'figure':
+
+                url_img = i.contents[0].attrs['src']
+                r = requests.get(url_img)
+                if r.status_code != requests.codes.ok:
+                    self.protocol.append({url_img: 'отсутсвует'})
+                else:
+                    self.protocol.append({url_img: 'доступна'})
+
+                try:
                     self.list_body.append(
-                            {
+                        {
+                            "type": "image",
+                            "data": {
+                                "file": {
+                                    "url": str(i.contents[0].attrs['src'])
+                                },
+                                "caption": "need to be correct parse",
+                                "withBorder": "false",
+                                "withBackground": "false",
+                                "stretched": "true"
+                            }
+                        }
+                                          )
+
+                except IndexError:
+                    print(f'отсутсвует значение{i}')
+            elif i.name == 'h2':
+                self.list_body.append(
+                        {
                             "type": "header",
                             "data": {
                                 "text": str(i.text),
                                 "level": 2
                             }
                         }
-                    )
-                else:
-                #self.list_body.append({f'"type" :unknown tag': i.text})
-                    pass
-
-            self.article_body.update(
-                {
-                  "time":  date,
-                  "blocks": self.list_body,
-                   "version": "0.01"
-                }
                 )
+            else:
+                #self.list_body.append({f'"type" :unknown tag': i.text})
+                pass
 
-            return self.article_body
+        self.article_body.update(
+            {
+                "time":  date,
+                "blocks": self.list_body,
+                "version": "0.01"
+             }
+        )
 
-#j = json.dumps(list_root)
+        with open('rep.html','w') as File:
+            File.write('<html>')
+            File.write('<body>')
+            File.write('<table>')
+
+            for i in self.protocol:
+                File.write(f'<p> {i} </p>')
+
+            File.write('</table>')
+            File.write('</body>')
+            File.write('</html>')
 
 
-#a = SearchArticles('/home/hortus/PycharmProjects/amocrm/app.db')
-b = SearchArticles('/home/hortus/PycharmProjects/amocrm/app.db')
-#result = a.get_article_content()
-result2 = b.get_article_content_all()
-#test = a.parse_content(date=result[0], content=result[1])
 
 
-test2 = b.parse_content()
-print(json.dumps(test2))
-pprint.pprint(test2)
 
+
+        return self.article_body
+
+
+#urlretrieve(url, file_path)
+a = SearchArticles('/home/hortus/PycharmProjects/amocrm/app.db')
+result = a.get_article_content()
+test = a.parse_content(date=result[0], content=result[1])
+print(json.dumps(test))
