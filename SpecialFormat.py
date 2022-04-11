8a33c0b7a7f19d892fb452069befacbe9d556c5e
Алексей Садовой <sapoko.stv@yandex.ru>
2022-04-11 21:52:14 +0300

    формирование отчетов в html

--- SpecialFormat.py
+++ SpecialFormat.py
@@ -17,13 +17,12 @@ class SearchArticles:
         self.list_body = []
         self.count = 0
         self.result = None
-        self.protocol=[]
+        self.protocol = []
         self.jpg_list = []
         self.url_error = []
 
     def execute_one(self, article_id):
-        self.article_id = article_id
-        self.cursor.execute("SELECT content FROM cms_article_content WHERE article_id = ?", (self.article_id,))
+        self.cursor.execute("SELECT content FROM cms_article_content WHERE article_id = ?", (article_id,))
         self.result = self.cursor.fetchone()
         return self.result
 
@@ -36,7 +35,7 @@ class SearchArticles:
     def list_img(self):
         pass
 
-    def execute_all(self,):
+    def execute_all(self):
         self.cursor.execute("SELECT content FROM cms_article_content")
         self.result = self.cursor.fetchall()
         return self.result
@@ -47,6 +46,38 @@ class SearchArticles:
                                 WHERE a.article_id = 7""")
         return self.cursor.fetchone()
 
+    def checking_picture(self, url):
+        r = requests.get(url, stream=True)
+        for chunk in r.iter_content(chunk_size=256):
+            if chunk:
+                self.protocol.append(url)
+                return 'есть'
+            else:
+                self.url_error.append(url)
+                return 'Нет'
+
+    def check_url(self, i):
+        if i.contents and 'src' in i.contents[0].attrs:
+            url = (str(i.contents[0].attrs['src']))
+            clear_url = urllib.parse.urlsplit(url)
+            if clear_url.netloc == "onlinepatent.ru":
+                return 'есть'
+            else:
+                return 'нет'
+
+    def write_file(self, name_files, name_report):
+        with open(name_files, 'w') as File:
+            File.write('<html>')
+            File.write('<body>')
+            File.write('<table>')
+
+            for i in name_report:
+                File.write(f'<p> {i} </p>')
+
+            File.write('</table>')
+            File.write('</body>')
+            File.write('</html>')
+
     def parse_content(self, date, content):
         for content in self.get_article_all():
             clr = re.sub(r"[\\\r\\\n]", "", str(content[1]))
@@ -55,116 +86,57 @@ class SearchArticles:
             for i in data_string.body:
                 if i.name == 'p':
                     self.list_body.append({
-                            "type": "paragraph",
-                            "data": {
-                                "text": str(i.text),
-                                }
+                        "type": "paragraph",
+                        "data": {
+                            "text": str(i.text),
+                        }
                     })
                 elif i.name == 'figure':
 
-                    caption = (i.find('figcaption'))
-                    if caption is not None:
-                        print(caption.text, '_______________________________________')
-                    if i.contents and 'src' in i.contents[0].attrs:
-                        url = (str(i.contents[0].attrs['src']))
-                        clear_url = urllib.parse.urlsplit(url)
-                        if clear_url.netloc == "onlinepatent.ru":
-                            with requests.get(url, stream=True) as r:
-                                r.raise_for_status()
-                                count = 0
-                                for chunk in r.iter_content(chunk_size=256):
-                                    chunk = chunk
-                                    if chunk:
-                                        count += 1
-                                        print(count)
-                                    return 'ok'
+                    if self.check_url(i) == 'есть':
+                        if self.checking_picture(url=str(i.contents[0].attrs['src'])) == 'есть':
+                            self.list_body.append(
+                                {
+                                    "type": "image",
+                                    "data": {
+                                        "file": {
 
-
-                                print(r.iter_content)
-                                self.protocol.append(str(i.contents[0].attrs['src']))
-
-                                self.list_body.append(
-                                    {
-                                        "type": "image",
-                                        "data": {
-                                            "file": {
-
-                                                "url": str(i.contents[0].attrs)
-                                            },
-                                            "caption": "need to be correct parse",
-                                            "withBorder": "false",
-                                            "withBackground": "false",
-                                            "stretched": "true"
-                                        }
+                                            "url": str(i.contents[0].attrs)
+                                        },
+                                        "caption": "need to be correct parse",
+                                        "withBorder": "false",
+                                        "withBackground": "false",
+                                        "stretched": "true"
                                     }
-                                              )
-
-                            self.url_error.append(i)
-                    else:
-
-                        self.url_error.append(i)
-
+                                }
+                            )
 
                 elif i.name == 'h2':
                     self.list_body.append(
-                            {
-                                "type": "header",
-                                "data": {
-                                    "text": str(i.text),
-                                    "level": 2
-                                }
+                        {
+                            "type": "header",
+                            "data": {
+                                "text": str(i.text),
+                                "level": 2
                             }
+                        }
                     )
-
-
-                elif i.name == 'н':
-                    print('figcaption')
-
-                else:
-                    #self.list_body.append({f'"type" :unknown tag': i.text})
-                    pass
 
             self.article_body.update(
                 {
-                    "time":  date,
+                    "time": date,
                     "blocks": self.list_body,
                     "version": "0.01"
-                 }
+                }
             )
-
-
-            with open('rep.html', 'w') as File:
-                File.write('<html>')
-                File.write('<body>')
-                File.write('<table>')
-
-                for i in self.protocol:
-                    File.write(f'<p> {i} </p>')
-
-                File.write('</table>')
-                File.write('</body>')
-                File.write('</html>')
-
-            with open('rep_eroros.html', 'w') as File:
-                File.write('<html>')
-                File.write('<body>')
-                File.write('<table>')
-
-                for i in self.url_error:
-                    File.write(f'<p> {i} </p>')
-
-                File.write('</table>')
-                File.write('</body>')
-                File.write('</html>')
 
         pprint.pprint(self.url_error)
         return self.article_body
 
 
-
-#urlretrieve(url, file_path)
 a = SearchArticles('/home/hortus/PycharmProjects/amocrm/app.db')
 result = a.get_article_content()
 test = a.parse_content(date=result[0], content=result[1])
-
+a.write_file("norm.html", a.protocol)
+a.write_file('errors_url.html', a.url_error)
 print(json.dumps(test))
