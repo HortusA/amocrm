71dc4d5f9081b465d792c00a87ffd63050cf81fd
alex <sadovoy.a@gmail.com>
2022-04-30 21:56:06 +0300

    Отчет эластика

--- ES.py
+++ ES.py
@@ -16,33 +16,43 @@ list_body = []
 
 
 def get_article_all():
-        cursor.execute("""SELECT a.date, ac.content FROM cms_article_content ac
-                                LEFT JOIN cms_articles a on a.article_id = ac.article_id
-                                WHERE a.article_id""")
-        return cursor.fetchall()
+    cursor.execute("""SELECT a.date, ac.content FROM cms_article_content ac
+                            LEFT JOIN cms_articles a on a.article_id = ac.article_id
+                            WHERE a.article_id""")
+    return cursor.fetchall()
+
+def execute_all():
+    cursor.execute("SELECT content FROM cms_article_content")
+    return cursor.fetchall()
 
 
 def create_index_es():
     count = 0
-    for content in get_article_all():
+    for content in execute_all():
+
         count += 1
+        es.index(index='my_index', id=count, document={'text': (content)})
+    print(count)
 
-        es.index(index='my_index', id=count, document={'text': str(content)})
-
-
-def search_id():
-    res = es.get(index="my_index", id=10)
-    print(res['_source'])
+#def search_id():
+ #   res = es.get(index="my_index", id=10)
+ #   print(res['_source'])
 
 
 def search_text():
-    resp = es.search(index="my_index", query={"match_all": {}})
-    print("Got %d Hits:" % resp['hits']['total']['value'])
-    for hit in resp['hits']['hits']:
-        print(hit["_source"])
+    body = {
+
+
+    }
+
+
+    #resp = es.search(index="my_index", query={"match_all": {}})
+    resp = es.search(index="my_index", body={'query': {'match': {'text': 'Китай'}}})
+
+    print(resp)
 
 
 create_index_es()
-search_id()
+
 search_text()
 
